13731fb652eaad0de8ab4587e7bc719e2df13553
alfayad-codeteak <alfayad@codeteak.com>
2025-07-28 19:28:40 +0530

    dfs

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -121,7 +121,7 @@ export function HeroSection() {
                     clipRule="evenodd"
                   />
                 </svg>
-                <span className="font-medium">Free Delivery Over    ₨ 250</span>
+                <span className="font-medium">Free Delivery Over ₨ 250</span>
               </div>
             </div>
           </div>
@@ -171,7 +171,7 @@ export function HeroSection() {
                   <svg className="w-5 h-5 text-primary flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                   </svg>
-                  <span className="font-medium">Free Delivery Over QR 50</span>
+                  <span className="font-medium">Free Delivery Over ₨ 250</span>
                 </div>
               </div>
               
